da56322216c731da5419ee5933d13626af7c007c
naturespaceboy <117329008+naturespaceboy@users.noreply.github.com>
2023-10-31 07:04:55 +0000

    Update jaamu.js

--- jaamu.js
+++ jaamu.js
@@ -1,41 +1,46 @@
 let score = 0;
-let timeRemaining = 60; 
+let timeRemaining = 60;
 
 const monkey = document.getElementById('monkey');
 const banana = document.getElementById('banana');
 const scoreElement = document.getElementById('score');
 const timerElement = document.getElementById('timer');
 
-
 function updateTimer() {
     timerElement.textContent = `Time: ${timeRemaining}s`;
 
     if (timeRemaining > 0) {
         timeRemaining--;
-        setTimeout(updateTimer, 1000); 
+        setTimeout(updateTimer, 1000);
     } else {
-        
         alert(`Game over! Your score is ${score}`);
         resetGame();
     }
 }
 
-// Start the game timer
 updateTimer();
 
-banana.addEventListener('animationiteration', () => {
+function moveBanana() {
     let randomPosition = Math.random() * 300;
     banana.style.left = `${randomPosition}px`;
-});
+
+    requestAnimationFrame(moveBanana);
+}
+
+moveBanana();
 
 document.addEventListener('keydown', (event) => {
-    let monkeyPosition = window.getComputedStyle(monkey).getPropertyValue('left');
-    monkeyPosition = parseInt(monkeyPosition);
+    let monkeyPosition = window.getComputedStyle(monkey);
+    let monkeyLeft = parseFloat(monkeyPosition.left);
 
-    if (event.key === 'ArrowLeft' && monkeyPosition > 0) {
-        monkey.style.left = `${monkeyPosition - 20}px`; 
-    } else if (event.key === 'ArrowRight' && monkeyPosition < 700) {
-        monkey.style.left = `${monkeyPosition + 20}px`;
+    if (event.key === 'ArrowLeft') {
+        if (monkeyLeft > 0) {
+            monkey.style.left = `${monkeyLeft - 20}px`;
+        }
+    } else if (event.key === 'ArrowRight') {
+        if (monkeyLeft < 320) {
+            monkey.style.left = `${monkeyLeft + 20}px`;
+        }
     }
 
     checkCollision();
@@ -45,7 +50,15 @@ function checkCollision() {
     let monkeyPosition = window.getComputedStyle(monkey);
     let bananaPosition = window.getComputedStyle(banana);
 
-    if (parseInt(bananaPosition.top) >= 700 && parseInt(bananaPosition.left) >= parseInt(monkeyPosition.left) && parseInt(bananaPosition.left) <= (parseInt(monkeyPosition.left) + 80)) {
+    let monkeyLeft = parseFloat(monkeyPosition.left);
+    let monkeyRight = monkeyLeft + parseFloat(monkeyPosition.width);
+    let bananaLeft = parseFloat(bananaPosition.left);
+
+    if (
+        parseFloat(bananaPosition.top) >= 370 &&
+        bananaLeft >= monkeyLeft &&
+        bananaLeft <= monkeyRight
+    ) {
         score++;
         scoreElement.textContent = `Score: ${score}`;
     }
